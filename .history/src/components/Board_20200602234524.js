e535afbc37faa5506558f3c877b6fabf849e7fa0
Sean <psytst@nottingham.ac.uk>
2020-06-02 23:46:13 +0700

    first commit

---
+++ .history/src/components/Board_20200602234524.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Square from "./Square";
+import { Row, Col } from "react-bootstrap/";
+import * as mySetting from "../settings";
+
+export default function Board({ board, onClick, enabled }) {
+  let renderSquare = (row) => {
+    let rowSquare = [];
+    board.forEach((element) => {
+      if (element.id[0] === row)
+        rowSquare.push(
+          <Col sm={4} style={mySetting.styles.col} key={element.id}>
+            <Square
+              value={element.value}
+              onClick={() => onClick(element, false)}
+            />
+          </Col>
+        );
+    });
+    return rowSquare;
+  };
+
+  return (
+    <>
+      <Row
+        style={
+          (mySetting.styles.row, { pointerEvents: enabled ? "auto" : "none" })
+        }
+      >
+        {renderSquare(0)}
+      </Row>
+      <Row
+        style={
+          (mySetting.styles.row, { pointerEvents: enabled ? "auto" : "none" })
+        }
+      >
+        {renderSquare(1)}
+      </Row>
+      <Row
+        style={
+          (mySetting.styles.row, { pointerEvents: enabled ? "auto" : "none" })
+        }
+      >
+        {renderSquare(2)}
+      </Row>
+    </>
+  );
+}
